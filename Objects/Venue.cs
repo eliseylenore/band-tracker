22d63b60f64f33d35d05d1413894ffa5c701117b
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-03 10:30:39 -0800

    Pass delete test

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -175,7 +175,22 @@ namespace BandTracker
 
         public void Delete()
         {
-            
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM venues WHERE id = @VenueId;", conn);
+
+            SqlParameter venueId = new SqlParameter();
+            venueId.ParameterName = "@VenueId";
+            venueId.Value = this._id;
+            cmd.Parameters.Add(venueId);
+
+            cmd.ExecuteNonQuery();
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
         }
 
         public static void DeleteAll()
