590887d3ec51d6f59be67a255f34893413b6f71d
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-03 11:21:29 -0800

    Add HomeModules.cs, add index page

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace BandTracker
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["index.cshtml"];
+        }
+    }
+}
