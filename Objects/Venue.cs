049019e3be4885379a5974614b9e8a2df5c4564a
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-03 09:37:52 -0800

    Fail GetAll Test

---
+++ Objects/Venue.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+    public class Venue
+    {
+        private int _id;
+        private string _name;
+
+        public Venue(string Name, int Id = 0)
+        {
+            _name = Name;
+            _id = Id;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetName().GetHashCode();
+        }
+
+        public override bool Equals(System.Object otherVenue)
+        {
+            if(!(otherVenue is Venue))
+            {
+                return false;
+            }
+            else
+            {
+                Venue newVenue = (Venue) otherVenue;
+                bool idEquality = (this.GetId() == newVenue.GetId());
+                bool nameEquality = (this.GetName() == newVenue.GetName());
+                return (idEquality && nameEquality);
+            }
+        }
+
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO venues(name) OUTPUT INSERTED.id VALUES( @VenueName);", conn);
+
+            SqlParameter venueName = new SqlParameter();
+            venueName.ParameterName = "@VenueName";
+            venueName.Value = this.GetName();
+            cmd.Parameters.Add(venueName);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+        public static List<Venue> GetAll()
+        {
+            // List<Venue> AllVenues = new List<Venue> {};
+            //
+            // SqlConnection conn = DB.Connection();
+            // conn.Open();
+            //
+            // SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+            // SqlDataReader rdr = cmd.ExecuteReader();
+            //
+            // while(rdr.Read())
+            // {
+            //     int venueId = rdr.GetInt32(0);
+            //     string venueName = rdr.GetString(1);
+            //
+            //     Venue newVenue = new Venue(venueName, venueId);
+            //     AllVenues.Add(newVenue);
+            // }
+            //
+            // if(rdr != null)
+            // {
+            //     rdr.Close();
+            // }
+            // if(conn != null)
+            // {
+            //     conn.Close();
+            // }
+
+            Venue newVenue = new Venue("Hey");
+            List<Venue> AllVenues = new List<Venue>{newVenue};
+            return AllVenues;
+        }
+
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM venues;", conn);
+            SqlParameter bandIdParameter = new SqlParameter();
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+    }
+}
---
+++ Tests/venuetest.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+    public class VenueTest : IDisposable
+    {
+        public VenueTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void GetAll_VenueEmptyAtFirst_true()
+        {
+            int result = Venue.GetAll().Count;
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Equals_ReturnsTrueForSameVenueName_true()
+        {
+            Venue firstVenue = new Venue("The Showbox");
+            Venue secondVenue = new Venue("The Showbox");
+
+            Assert.Equal(firstVenue, secondVenue);
+        }
+
+        [Fact]
+        public void GetAll_ReturnsAllVenues_List()
+        {
+            Venue newVenue = new Venue ("The Showbox");
+            newVenue.Save();
+
+            List<Venue> expectedResult = new List<Venue>{newVenue};
+            List<Venue> actual = Venue.GetAll();
+
+            Assert.Equal(expectedResult, actual);
+        }
+
+        public void Dispose()
+        {
+            Band.DeleteAll();
+            Venue.DeleteAll();
+        }
+    }
+}
